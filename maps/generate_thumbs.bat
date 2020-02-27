6455f21201f2d44718d1f61369abcc23556c0fa7
Jerry Malmström <jerry.malmstrom@borealisgroup.com>
2020-02-27 22:39:56 +0100

    tmxrasterizer

--- maps/generate_thumbs.bat
+++ maps/generate_thumbs.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 ECHO %1
 "c:\Program Files\Tiled\tmxrasterizer.exe" --size 300 --hide-layer "management" "%1" "%1.thumb.png"
+"C:\Program Files (x86)\Tiled\tmxrasterizer.exe" --size 300 --hide-layer "management" "%1" "%1.thumb.png"
